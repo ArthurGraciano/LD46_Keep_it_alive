11315e6ee9df044eae436d88925cf8dcd4b121f8
lucianapdp <lucianapdp@gmail.com>
2020-04-18 17:28:04 -0300

    Buttons Scripts - Play e Credits

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void PlayGame()
+    {
+        Panel.SetActive(false);
+    }
+}
