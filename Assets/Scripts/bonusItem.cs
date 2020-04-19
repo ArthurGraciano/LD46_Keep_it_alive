16b4dfd7f114e1cb4fd9e4170fe7ca0e5bac8d8a
voromwe <arthur_graciano@hotmail.com>
2020-04-19 18:53:41 -0300

    bonus item follow

--- Assets/Scripts/bonusItem.cs
+++ Assets/Scripts/bonusItem.cs
@@ -16,37 +16,56 @@ public class bonusItem : MonoBehaviour
     [SerializeField]
     private Vector2 _direction;
 
+    private bool dirChange;
+
     // Start is called before the first frame update
     void Start()
-    {   
-        
+    {
+        dirChange = false;
         itemCollider = GetComponent<CircleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        player = GameObject.FindGameObjectWithTag("Astronaut").transform;
         transform.position = RespawnPointsList.respawnMethod();
         randomSpawnRange = Random.Range(0, 3);
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        _direction = (directions[randomSpawnRange] - rb.position).normalized;
-        rb.MovePosition(rb.position + _direction * speed * Time.fixedDeltaTime);
-        if(rb.position == directions[randomSpawnRange])
-        {
-            Destroy(gameObject);
-        }
+        StartCoroutine(changeDirection());
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag.Equals("Astronaut"))
         {
-            planet.healthAmount += 5;
+            planet.healthAmount += 10;
             Destroy(gameObject);
+
+        }else if(collision.tag.Equals("bullet Astronaut"))
+        {
+            dirChange = true;
+            Destroy(collision.gameObject);
         }
     }
 
+    IEnumerator changeDirection()
+    {   
+        if(dirChange == true) 
+        { 
+        _direction = player.position;
+        rb.MovePosition(rb.position - _direction * speed * Time.fixedDeltaTime);
+            yield return new WaitForSeconds(5f);
+            dirChange = false;
+        }
+        else if(dirChange == false)
+        {
+            _direction = (directions[randomSpawnRange] - rb.position);
+            rb.MovePosition(rb.position + _direction * speed * Time.fixedDeltaTime);
+        }
+    }
 
 }
