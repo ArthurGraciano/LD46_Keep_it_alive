7edc58433309fd7804cf12bd37cb797c6e180acd
lucianapdp <lucianapdp@gmail.com>
2020-04-19 16:20:15 -0300

    EndGamePopUpManager script

---
+++ Assets/EndGamePopupManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGamePopupManager : MonoBehaviour
+{
+    public GameObject gameOver;
+    public GameObject gameWin;
+    public GameObject planet;
+    public static float healthAmount;
+    void Update()
+    {
+        Health();
+         void Health();
+     
+        if (healthAmount <= 0)
+        {
+            Time.timeScale = 0f;
+            gameOver.SetActive(true);
+        }
+
+        else if (healthAmount >=100)
+        {
+            Time.timeScale = 0f;
+            gameWin.SetActive(true);
+        }
+
+
+    }
